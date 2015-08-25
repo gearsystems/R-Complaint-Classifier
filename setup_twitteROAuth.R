3e9d5a041e8b5c5a4d669b4d8c07658d10a955a7
Aman Alam <aman_alam@rocketmail.com>
2015-08-25 23:28:06 +0530

    Setting up Twitter

---
+++ setup_twitteROAuth.R
@@ -0,0 +1,20 @@
+library(twitteR)
+
+
+setup_twitteROAuth <- function( key, secret, token, token_secret ) {
+  
+#Process to register ROAuth for a session
+#-------------------------------------------------------------------------------------------------------------
+api_key <- key
+
+api_secret <- secret
+
+access_token <- token
+
+access_token_secret <- token_secret
+
+setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+
+}
+
+#-------------------------------------------------------------------------------------------------------------
